a7db3cddd9a095c8b3c7166e329c6ae376ca916d
Pei Yao,Xu <u0124008@nkfust.edu.tw>
2015-12-19 13:35:17 +0800

    MemberInfo擴充
    更簡單取得MemberInfo

--- src/AspNet5Template/Extensions/EntityFramework/IQueryableExtension.cs
+++ src/AspNet5Template/Extensions/EntityFramework/IQueryableExtension.cs
@@ -12,7 +12,14 @@ using System.Threading.Tasks;
 namespace AspNet5Template.Extensions.EntityFramework{
     public static class IQueryableExtension {
         //實作LazyLoad擴充
-        public static IQueryable LazyLoad<TEntity>(this IQueryable<TEntity> source) where TEntity : class {
+        /// <summary>
+        /// 完整載入屬性
+        /// </summary>
+        /// <typeparam name="TEntity"></typeparam>
+        /// <param name="source">擴充對象</param>
+        /// <param name="Level">目標階層數量(預設僅目前物件屬性)</param>
+        /// <returns></returns>
+        public static IQueryable FullInclude<TEntity>(this IQueryable<TEntity> source,int Level = 0) where TEntity : class {
             Type GenericType = source.GetType().GetGenericArguments()?[0];
 
             PropertyInfo[] Properties = GenericType.GetProperties();
@@ -36,12 +43,16 @@ namespace AspNet5Template.Extensions.EntityFramework{
                 #endregion
 
                 List<Type> typeList = new List<Type>();
-                result = result.Include(func).ThenLazyLoad<TEntity>(func2, typeList);//解決單層讀取問題
+                if (Level > 0) {
+                    result = result.Include(func).ThenFullInclude<TEntity>(func2, typeList,Level - 1);//解決單層讀取問題
+                } else {
+                    result = result.Include(func);
+                }
             }
             return result;
         }
 
-        private static IIncludableQueryable<TEntity, dynamic> ThenLazyLoad<TEntity>(this IIncludableQueryable<TEntity, dynamic> source, Expression<Func<dynamic, object>> Func, List<Type> typeList) where TEntity : class {
+        private static IIncludableQueryable<TEntity, dynamic> ThenFullInclude<TEntity>(this IIncludableQueryable<TEntity, dynamic> source, Expression<Func<dynamic, object>> Func, List<Type> typeList,int Level) where TEntity : class {
             //var temp = Func.Compile().Invoke(source.First());
 
             MemberExpression e = (MemberExpression)Func.Body;
@@ -69,7 +80,11 @@ namespace AspNet5Template.Extensions.EntityFramework{
                 List<Type> typeList2 = new List<Type>(typeList.ToArray());
                 typeList2.Add(type);//Function Return Type
 
-                result = result.ThenInclude(func).ThenLazyLoad(func,typeList2);//.ThenLazyLoad<TEntity>(func);//解決單層讀取問題
+                if (Level > 0) {
+                    result = result.ThenInclude(func).ThenFullInclude(func, typeList2, Level - 1);
+                } else {
+                    result = result.ThenInclude(func);//.ThenLazyLoad<TEntity>(func);//解決單層讀取問題
+                }
             }
             return result;
         }
